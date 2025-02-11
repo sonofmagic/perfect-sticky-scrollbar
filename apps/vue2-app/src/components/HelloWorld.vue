554480d9c5dfd0c9b697f4883f682ef6f2beb8b5
ice breaker <1324318532@qq.com>
2025-02-11 17:25:50 +0800

    chore: adas

--- apps/vue2-app/src/components/HelloWorld.vue
+++ apps/vue2-app/src/components/HelloWorld.vue
@@ -1,4 +1,10 @@
 <script lang="ts" setup>
+import PerfectScrollbar from 'perfect-sticky-scrollbar'
+import { onMounted, ref } from 'vue'
+import 'perfect-sticky-scrollbar/style.css'
+
+const tableRef = ref()
+
 const tableData = Array.from({ length: 100 }).map(() => {
   return {
     date: '2016-05-02',
@@ -9,12 +15,26 @@ const tableData = Array.from({ length: 100 }).map(() => {
     zip: 200333,
   }
 })
+
+onMounted(() => {
+  const bodyWrapper = tableRef.value.$el.querySelector('.el-table__body-wrapper') as HTMLDivElement
+  // bodyWrapper.style.position = 'relative'
+
+  if (bodyWrapper) {
+    const ps = new PerfectScrollbar(bodyWrapper, {
+      stickyXScrollbar: true,
+      stickyXScrollbarOffset: 40,
+    })
+    bodyWrapper.style.overflowX = 'hidden'
+    console.log(ps)
+  }
+})
 </script>
 
 <template>
   <div class="container mx-auto">
     <div class="w-160">
-      <el-table class="sticky-x" :data="tableData">
+      <el-table ref="tableRef" class="sticky-x" :data="tableData">
         <el-table-column prop="date" label="日期" width="150" />
         <el-table-column prop="name" label="姓名" width="120" />
         <el-table-column prop="province" label="省份" width="120" />
@@ -47,5 +67,8 @@ const tableData = Array.from({ length: 100 }).map(() => {
 </template>
 
 <style lang="scss">
-
+.ps__rail-x,
+.ps__rail-y {
+  opacity: 0.6;
+}
 </style>
