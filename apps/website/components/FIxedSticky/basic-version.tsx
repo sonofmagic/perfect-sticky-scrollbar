83ab17786891aae5cec6b359462428b210bb8eba
ice breaker <1324318532@qq.com>
2025-02-11 11:32:39 +0800

    chore: axas

---
+++ apps/website/components/FIxedSticky/basic-version.tsx
@@ -0,0 +1,93 @@
+import type { CSSProperties, ReactNode } from 'react'
+import type { CommonProps } from './types'
+import React from 'react'
+import StickyRenderProp from './render-props-version'
+
+export interface Props extends CommonProps {
+  /**
+   * Class name to be applied to the sticky element when it is fixed.
+   * @default {}
+   */
+  stickyStyle: CSSProperties
+  /**
+   * Class name to be applied to the sticky element when it is fixed.
+   * @default 'sticky'
+   */
+  stickyClassName: string
+  /**
+   * Class name to be applied to the wrapper element.
+   * @default ''
+   */
+  wrapperClassName: string
+  children?: ReactNode
+}
+
+export const defaultProps = {
+  ...StickyRenderProp.defaultProps,
+  stickyClassName: 'sticky',
+  wrapperClassName: '',
+  stickyStyle: {},
+}
+function Sticky(props: Partial<Props>) {
+  const {
+    // props for StickyRenderProp
+    mode,
+    onFixedToggle,
+    hideOnBoundaryHit,
+    offsetTransforms,
+    disabled,
+    boundaryElement,
+    scrollElement,
+    bottomOffset,
+    topOffset,
+    positionRecheckInterval,
+    children,
+    isIOSFixEnabled,
+    dontUpdateHolderHeightWhenSticky,
+
+    // own props
+    wrapperClassName,
+    stickyClassName,
+    stickyStyle,
+
+    // rest of the props that we will forward to wrapper
+    ...rest
+  } = {
+    ...defaultProps,
+    ...props,
+  }
+
+  return (
+    <StickyRenderProp
+      mode={mode}
+      onFixedToggle={onFixedToggle}
+      hideOnBoundaryHit={hideOnBoundaryHit}
+      offsetTransforms={offsetTransforms}
+      disabled={disabled}
+      boundaryElement={boundaryElement}
+      scrollElement={scrollElement}
+      bottomOffset={bottomOffset}
+      topOffset={topOffset}
+      positionRecheckInterval={positionRecheckInterval}
+      isIOSFixEnabled={isIOSFixEnabled}
+      dontUpdateHolderHeightWhenSticky={dontUpdateHolderHeightWhenSticky}
+    >
+      {({ isFixed, wrapperStyles, wrapperRef, holderStyles, holderRef }) => (
+        <div {...rest} ref={holderRef} style={holderStyles}>
+          <div
+            {...rest}
+            className={`${wrapperClassName} ${isFixed ? stickyClassName : ''}`}
+            style={
+              isFixed ? { ...wrapperStyles, ...stickyStyle } : wrapperStyles
+            }
+            ref={wrapperRef}
+          >
+            {children}
+          </div>
+        </div>
+      )}
+    </StickyRenderProp>
+  )
+}
+
+export default Sticky
---
+++ apps/website/components/FIxedSticky/index.tsx
@@ -0,0 +1,10 @@
+import RenderPropSticky from './render-props-version';
+
+import BasicSticky from './basic-version';
+
+
+export {
+  RenderPropSticky
+};
+
+export default BasicSticky;
